7711d5df7cfbf91419ff242db921c9ef42a07216
Kuldeep <kuldeepmer11@gmail.com>
2023-11-05 22:10:20 +0530

    first

---
+++ components/main/Hero.tsx
@@ -0,0 +1,24 @@
+"use client"
+import React from 'react'
+import { useRef } from 'react';
+import HeroContainer from '../sub/HeroContainer';
+
+const Hero = () => {
+  // const video = useRef<HTMLVideoElement>();
+  return (
+    <div className="relative flex flex-col h-full w-full" >
+       <video
+       autoPlay
+       muted
+       loop
+       className='rotate-180 absolute top-[-340px] h-full w-full left-0 z-[1] object-cover'
+       >
+        <source src='/blackhole.webm' type='video/webm'/>
+       </video>
+
+       <HeroContainer/>
+    </div>
+  )
+}
+
+export default Hero
